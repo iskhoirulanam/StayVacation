727f2f5d83fe86a74428cd6334a220b9f247f63b
iskhoirulanam <iskhoirul.anam@students.amikom.ac.id>
2020-10-23 23:23:10 +0700

    first-commit

---
+++ src/pages/LandingPages.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import moduleName from 'elements/buttons'
+
+export class LandingPages extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default LandingPages
+
